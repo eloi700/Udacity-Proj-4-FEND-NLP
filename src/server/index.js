eed6a8e5cd982053972513f0b401b45bbf9682a0
Cielo <macielo.tablate@gmail.com>
2022-06-03 19:51:10 +0200

    fix errors

--- src/server/index.js
+++ src/server/index.js
@@ -15,7 +15,7 @@ app.get("/", function (req, res) {
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
+app.listen(process.env.PORT || 8081, function () {
   console.log("Example app listening on port 8081!");
 });
 
